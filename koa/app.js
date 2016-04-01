78485b07dc21ecdaf2dd7cd5f70ea76566cd7722
周鹏 <zhoupeng@xiaomi.com>
2016-04-01 18:38:22 +0800

    加入了koa的实现版本

---
+++ koa/app.js
@@ -0,0 +1,34 @@
+var koa = require('koa')
+,   session = require('koa-session')
+,   router = require('koa-router')
+,   mount = require('koa-mount')
+,   koaqs = require('koa-qs')
+,   accesslog = require('koa-accesslog')
+
+var Grant = require('grant-koa')
+,   grant = new Grant(require('./config.json'))
+
+var app = koa()
+app.use(accesslog())
+//REQUIRED
+app.keys = ['grant']
+app.use(session(app))
+// mount grant
+app.use(mount(grant))
+// other middlewares
+app.use(router(app))
+koaqs(app)
+
+app.get('/handle_facebook_callback', function *(next){
+    console.log(this.query)
+    this.body = JSON.stringify(this.query, null, 2)
+})
+
+app.get('/handle_twitter_callback', fucntion *(next){
+    console.log(this.query)
+    this.body = JSON.stringify(this.query, null, 2)
+})
+
+app.listen(3000, function(){
+    console.log('Koa server listening on port ' + 3000)
+})
